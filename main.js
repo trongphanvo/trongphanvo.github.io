f812f82ce991c331b9c7d60830782d699186ad17
trongphanvo <116131359+trongphanvo@users.noreply.github.com>
2022-10-19 08:33:00 +0700

    test

---
+++ main.js
@@ -0,0 +1,36 @@
+// const socket = io('http://localhost:3000')
+
+function openStream(){
+    return navigator.mediaDevices.getUserMedia({audio: true, video: true});
+}
+
+function playStream(idVideo, stream) {
+    const video = document.getElementById(idVideo);
+    video.srcObject= stream
+    video.play()
+}
+
+var peer = new Peer();
+peer.on('open', id=> $('#idPeer').append(id)) 
+
+$('#call').click(()=>{
+    const id = $('#remoteid').val()
+    openStream().then(stream => {
+        playStream('video1',stream)
+        const call = peer.call(id,stream)
+        call.on('stream', remoteStream=> playStream('video2',remoteStream))
+    })
+})
+
+peer.on('call', call =>{
+    openStream().then(stream =>{
+       playStream('video1',stream)
+        call.answer(stream)
+        call.on('stream', remoteStream=> playStream('video2',remoteStream))
+    })
+})
+
+
+$('#signup').click(()=>{
+    
+})
