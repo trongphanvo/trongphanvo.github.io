ee3c3245809c186d390e4c815e2f3ba11110e6a9
trongphanvo <116131359+trongphanvo@users.noreply.github.com>
2022-10-24 10:43:31 +0700

    fix

--- main.js
+++ main.js
@@ -1,36 +1,60 @@
+function handleSoundVideo(so, vi) {
+  const option = {
+    audio: so,
+    video: vi,
+  };
+  return option;
+}
 
-
-function openStream(){
-    return navigator.mediaDevices.getUserMedia({audio: true, video: false});
+function openStream() {
+  return navigator.mediaDevices.getUserMedia(handleSoundVideo(true, true));
 }
 
 function playStream(idVideo, stream) {
-    const video = document.getElementById(idVideo);
-    video.srcObject= stream
-    video.play()
+  const video = document.getElementById(idVideo);
+  video.srcObject = stream;
+  video.play();
 }
 
 var peer = new Peer();
-peer.on('open', id=> $('#idPeer').append(id)) 
+peer.on("open", (id) => $("#idPeer").append(id));
 
-$('#call').click(()=>{
-    const id = $('#remoteid').val()
-    openStream().then(stream => {
-        playStream('video1',stream)
-        const call = peer.call(id,stream)
-        call.on('stream', remoteStream=> playStream('video2',remoteStream))
-    })
-})
+let localStream;
 
-peer.on('call', call =>{
-    openStream().then(stream =>{
-       playStream('video1',stream)
-        call.answer(stream)
-        call.on('stream', remoteStream=> playStream('video2',remoteStream))
-    })
-})
+$("#call").click(() => {
+  const id = $("#remoteid").val();
+  openStream().then((stream) => {
+    localStream = stream;
+    playStream("video1", stream);
+    const call = peer.call(id, stream);
+    call.on("stream", (remoteStream) => playStream("video2", remoteStream));
+  });
+});
 
-
-$('#signup').click(()=>{
-    
-})
+peer.on("call", (call) => {
+  openStream().then((stream) => {
+    playStream("video1", stream);
+    call.answer(stream);
+    call.on("stream", (remoteStream) => playStream("video2", remoteStream));
+  });
+});
+let stateVideo = true;
+$("#signup").click(() => {
+  if (stateVideo) {
+    localStream.getVideoTracks()[0].enabled = false;
+    stateVideo = false;
+  } else {
+    localStream.getVideoTracks()[0].enabled = true;
+    stateVideo = true;
+  }
+});
+let stateSound = true;
+$("#signup2").click(() => {
+  if (stateSound) {
+    localStream.getAudioTracks()[0].enabled = false;
+    stateSound = false;
+  } else {
+    localStream.getAudioTracks()[0].enabled = true;
+    stateSound = true;
+  }
+});
